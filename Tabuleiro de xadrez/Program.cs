9ec579b24e056fd70a0ba2cfce35bfb65de8fb13
IuriCaliman <30376056+IuriCaliman@users.noreply.github.com>
2019-08-09 17:21:30 -0300

    Add files via upload
    Acrescentado uma matriz, personagem móvel (em partes)

--- Tabuleiro de xadrez/Program.cs
+++ Tabuleiro de xadrez/Program.cs
@@ -4,55 +4,88 @@ namespace Tabuleiro_de_xadrez
 {
     class Program
     {
-        static void troca(ref char a, ref char b, out string fez)
+        static void inicializaMatriz(ref char[][] mapa)
+        {
+            char a = 'X';
+            char b = 'O';
+            for (int i = 0; i < 8; i++)
+            {
+                for (int o = 0; o < 8; o++)
+                {
+                    if (o % 2 == 0)
+                    {
+                        mapa[i][o] = a;
+                    }
+                    else
+                    {
+                        mapa[i][o] = b;
+                    }
+                }
+                Program.troca(ref a, ref b);
+            }
+        }
+
+        //Para montar o tabuleiro, precisa alternar se o X vem primeiro ou o O, para imitar o branco e preto foi utilizado X e O, respectivamente
+        static void troca(ref char a, ref char b)
         {
             char aux;
             aux = a;
             a = b;
             b = aux;
-
-            fez = "              Aproveite o seu tabuleiro!";
         }
+
         static void Main(string[] args)
         {
-            char start = '|';
-            char a = 'X';
-            char b = 'O';
-            string fez;
-            ConsoleKeyInfo tecla;
-            char[][] mapa = new char[8][];
+            char start = '|'; //Boneco que vai andar
+            int posX = 0;//Coordenadas do boneco na matriz
+            int posY = 0;
+
+            ConsoleKeyInfo tecla; //Tecla apertada pelo usuário
+            char[][] mapa = new char[8][]; //Tabuleiro, criando a primeira coluna
+            //Cria as linhas da matriz
             for (int i = 0; i < 8; i++)
             {
                 mapa[i] = new char[8];
             }
-            
+            //
+
+            Program.inicializaMatriz(ref mapa);
+
+            //Apresenta a matriz até que o usuário aperte backspace
             do
             {
+
                 Console.Clear();
-                Console.Write(start);
                 for (int i = 0; i < 8; i++)
                 {
+                    Console.Write('\t');
                     for (int o = 0; o < 8; o++)
                     {
+                        if (posX == i && posY == o)
+                        {
+                            Console.Write(start); //Lugar onde o personagem atualmente esta
+                        }
                         if (o % 2 == 0)
                         {
-                            mapa[i][o] = a;
-                            Console.Write(a);
+                            Console.Write(mapa[i][o]);
                         }
                         else
                         {
-                            mapa[i][o] = b;
-                            Console.Write(b);
+                            Console.Write(mapa[i][o]);
                         }
+                        Console.Write('\t');
                     }
-                    Program.troca(ref a, ref b, out fez);
                     Console.WriteLine();
                 }
-
-                
-                //tecla = Console.ReadKey();
-
-            } while (true);
+                tecla = Console.ReadKey();
+                if (tecla.Key == ConsoleKey.DownArrow)
+                {
+                    if (posX+1 <= 7)
+                    {
+                        posX++;
+                    }
+                }
+            } while (tecla.Key != ConsoleKey.Backspace);
         }
     }
 
