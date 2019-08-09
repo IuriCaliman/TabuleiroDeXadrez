a4c79ecd3d0896830d4d8160654956ea819d6665
IuriCaliman <30376056+IuriCaliman@users.noreply.github.com>
2019-08-09 15:29:37 -0300

    Add files via upload
    Primeira versão

---
+++ Tabuleiro de xadrez/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Tabuleiro_de_xadrez
+{
+    class Program
+    {
+        static void troca(ref char a, ref char b, out string fez)
+        {
+            char aux;
+            aux = a;
+            a = b;
+            b = aux;
+
+            fez = "              Aproveite o seu tabuleiro!";
+        }
+        static void Main(string[] args)
+        {
+            char start = '|';
+            char a = 'X';
+            char b = 'O';
+            string fez;
+            ConsoleKeyInfo tecla;
+            char[][] mapa = new char[8][];
+            for (int i = 0; i < 8; i++)
+            {
+                mapa[i] = new char[8];
+            }
+            
+            do
+            {
+                Console.Clear();
+                Console.Write(start);
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int o = 0; o < 8; o++)
+                    {
+                        if (o % 2 == 0)
+                        {
+                            mapa[i][o] = a;
+                            Console.Write(a);
+                        }
+                        else
+                        {
+                            mapa[i][o] = b;
+                            Console.Write(b);
+                        }
+                    }
+                    Program.troca(ref a, ref b, out fez);
+                    Console.WriteLine();
+                }
+
+                
+                //tecla = Console.ReadKey();
+
+            } while (true);
+        }
+    }
+
+}
